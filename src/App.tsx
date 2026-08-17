2af70a1a35855d28d4a404b888fa4e01603fa266
romulovidal <romulo_vidal@live.com>
2026-08-16 21:12:29 -0300

    feat: add ATIS birthdays route

--- src/App.tsx
+++ src/App.tsx
@@ -91,6 +91,7 @@ const AppContent = () => {
             <Route path="/admin" element={<AdminPage />} />
             <Route path="/atis" element={<AtisPage />} />
             <Route path="/atis/destinatarios" element={<AtisPage />} />
+            <Route path="/atis/aniversariantes" element={<AtisPage />} />
             <Route path="/admin/cultos" element={<AdminCultoSelectionsPage />} />
             <Route path="/estudos/revistas" element={<PageTransition><RevistasPage /></PageTransition>} />
             <Route path="/app" element={<AppLanding />} />
